98af00abbf8a32647f51588eb00b5b10dac2ee6e
ManjuRepo <Manjukvespn@gmail.com>
2024-10-10 11:30:01 +0530

    ok

---
+++ src/OverRide/Day1.java
@@ -0,0 +1,29 @@
+package OverRide;
+
+class Day3 {
+
+	void Demo(int x, int y) {
+
+		System.out.println("Addition"+" "+ (x + y));
+
+	}
+}
+
+class Day2 extends Day3 {
+
+	void Demo(int x, int y) {
+
+		System.out.println("Addition"+" "+ (x+ y));
+		
+		super.Demo(45, 54);
+
+	}
+}
+
+public class Day1 {
+	public static void main(String[] args) {
+		Day2 day = new Day2();
+		day.Demo(9, 10);
+		
+	}
+}
