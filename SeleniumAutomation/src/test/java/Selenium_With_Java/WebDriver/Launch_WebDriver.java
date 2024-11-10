9da522bda166bf616f42fa8b575bda49121dafca
ManjuRepo <Manjukvespn@gmail.com>
2024-11-10 15:34:00 +0530

    OK

--- SeleniumAutomation/src/test/java/Selenium_With_Java/WebDriver/Launch_WebDriver.java
+++ SeleniumAutomation/src/test/java/Selenium_With_Java/WebDriver/Launch_WebDriver.java
@@ -1,22 +1,22 @@
 package Selenium_With_Java.WebDriver;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Launch_WebDriver {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Open Browser
-//		WebDriver Chromedriver = new ChromeDriver();
-		WebDriver Edgedriver = new EdgeDriver();
+		WebDriver Chromedriver = new ChromeDriver();
+//		WebDriver Edgedriver = new EdgeDriver();
 //		WebDriver Safaridriver = new SafariDriver();
 //		WebDriver Firefoxdriver = new FirefoxDriver();
 
 		// Navigate to specified URL
 		Thread.sleep(3000);
-		Edgedriver.get("https://www.youtube.com/");
+		Chromedriver.get("https://www.youtube.com/");
 
 		// Close Browser
-		Edgedriver.close();
+		Chromedriver.close();
 	}
 }
