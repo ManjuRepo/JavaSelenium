d603ee2d790c32fefe8472864643ed60dd96cdec
ManjuRepo <Manjukvespn@gmail.com>
2024-12-03 17:46:18 +0530

    ok

--- SeleniumAutomation/src/test/java/Selenium_With_Java/WebDriver/Launch_WebDriver.java
+++ SeleniumAutomation/src/test/java/Selenium_With_Java/WebDriver/Launch_WebDriver.java
@@ -1,14 +1,11 @@
 package Selenium_With_Java.WebDriver;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Launch_WebDriver {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Open Browser
 
-		WebDriver Chromedriver = new ChromeDriver();
+//		WebDriver Chromedriver = new ChromeDriver();
 //		WebDriver Edgedriver = new EdgeDriver();
 //		WebDriver Safaridriver = new SafariDriver();
 //		WebDriver Firefoxdriver = new FirefoxDriver();
